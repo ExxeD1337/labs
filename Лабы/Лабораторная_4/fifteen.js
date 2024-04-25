1311a0bc16f3c396d6b1ce0ed2e9b22f8a48263c
Saveliy Petrov <savych2000@gmail.com>
2024-04-25 18:17:13 +0300

    Пятнашки

--- Лабы/Лабораторная_4/fifteen.js
+++ Лабы/Лабораторная_4/fifteen.js
@@ -1,28 +1,28 @@
 $(document).ready(function(){
 	$("div div div").addClass("puzzlepiece");
 
-	//These will help position the divs properly in the puzzle area
+
     let posx = parseInt($("#puzzlearea").css("top"));
     let posy = parseInt($("#puzzlearea").css("left"));
 
-    //These are for the pics position
+   
     let picx = 0;
     let picy = 0;
     
-    //Positions for blank square
+   
     let blankx = 300;
     let blanky = 300;
 
 	let pieces = document.getElementsByClassName("puzzlepiece");
 
 	for(let i=0; i < pieces.length; i++){
-		//positioning the image on each div
+		
 		$(pieces[i]).css("background-position", picx+"px "+picy+"px");
 
 		picx -= 100;
 		if(picx%400 == 0){ picy -= 100; }
 
-		//Positioning each div
+		
 		$(pieces[i]).css("top", posy);
 		$(pieces[i]).css("left", posx);
 
@@ -33,23 +33,23 @@ $(document).ready(function(){
 			posx = parseInt($("#puzzlearea").css("top")); 
 		}
 
-		//Glow piece if hovered
+		
 		$(pieces[i]).on("mouseover", function(){
 			if(validate(this)){ $(this).addClass("movablepiece"); }
 		});
 
-		//If mouse leaves don't remove movable class
+		
 		$(pieces[i]).on("mouseleave", function(){
 			$(this).removeClass("movablepiece");
 		});
 
-		//Switch piece with blank if clicked
+		
 		$(pieces[i]).on("click", function(){
 			if(validate(this)){ switchTile(this); }
 		});
 	}
 
-	//Test if tiles are near blank tile
+	
 	let validate = function(piece){
 
 		if(((parseInt($(piece).css("top")) - blanky == 100 || parseInt($(piece).css("top")) - blanky == -100) && parseInt($(piece).css("left")) - blankx == 0)
@@ -60,7 +60,7 @@ $(document).ready(function(){
 		else{ return false; }
 	};
 
-	//function to switch
+
 	let switchTile = function(move){
 		let tempx = blankx;
 		let tempy = blanky;
